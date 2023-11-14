fdf1db61ccb2db9dee19d72d8d30d32511ea58fb
ysys <1243933010@qq.com>
2023-11-14 17:27:44 +0800

    新增基础插件

---
+++ utils/requestObj.js
@@ -0,0 +1,52 @@
+let requestObj = {
+	getMailOrderList:{
+		url:'/yoursender/api/mailOrder/getMailOrderList',
+		type:'GET'
+		
+	},
+	addLogisticOrder:{
+		url:'/yoursender/api/mailOrder/addLogisticOrder',
+		type:'POST'
+	},
+	wechatLogin:{
+		url:'/yoursender/api/wechat/login',
+		type:'GET',
+		otherGet:true
+	},
+	mailInfo:{
+		url:'/yoursender/api/mailOrder/mailInfo',
+		type:'GET',
+		otherGet:true
+	},
+	getRoutes:{
+		url:'/yoursender/api/mailOrder/getRoutes',
+		type:'GET'
+	},
+	infoByReceiptsNo:{
+		url:'/yoursender/api/mailOrder/infoByReceiptsNo',
+		type:'GET',
+		otherGet:true
+	},
+	testParams:{
+		url:'/yoursender/api/wechat/testParams',
+		type:'POST'
+	},
+	payOrder:{
+		url:'/yoursender/api/wechat/payOrder',
+		type:'POST'
+	},
+	getFreight:{
+		url:'/yoursender/api/mailOrder/getFreight',
+		type:'GET',
+	},
+	orderCancel:{
+		url:'/yoursender/api/mailOrder/orderCancel',
+		type:'GET',
+		otherGet:true
+	},
+	getNotification:{
+		url:'/yoursender/api/mailOrder/getNotification',
+		type:'GET'
+	}
+}
+export default requestObj;
