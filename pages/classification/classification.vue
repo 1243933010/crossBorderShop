3a92b41fe778ae8d49540a9a8de8d1db85e67977
ysys <1243933010@qq.com>
2023-11-15 09:34:00 +0800

    feat():分類頁面切圖

--- pages/classification/classification.vue
+++ pages/classification/classification.vue
@@ -1,11 +1,18 @@
 <template>
 	<view>
-		
+		<customHeader style="z-index: 0;"/>
+		<view style="position: absolute;top: 0;width: 100%;">
+			<customHeader />
+		</view>
+		<uni-easyinput prefixIcon="search" v-model="value" placeholder="左侧图标" @iconClick="iconClick">
+					</uni-easyinput>
 	</view>
 </template>
 
 <script>
+	import customHeader from '@/components/customHeader/customHeader.nvue'
 	export default {
+		components:{customHeader},
 		data() {
 			return {
 				
