f84c1942cbfc55becafba0c37aeb36fceea8f408
yglbhdy <1181330602@qq.com>
2023-11-15 09:33:23 +0800

    自定义头部的适配

--- App.vue
+++ App.vue
@@ -1,56 +1,61 @@
 <script>
-	export default {
-		onLaunch: function() {
-			console.log('App Launch')
-			let code =  uni.getLocale();
-			if(!code){
-				uni.setLocale('zh-Hans');
-			}
-			console.log(code)
-		},
-		onShow: function() {
-			console.log('App Show')
-		},
-		onHide: function() {
-			console.log('App Hide')
+export default {
+	onLaunch: function () {
+		console.log("App Launch");
+		let code = uni.getLocale();
+		if (!code) {
+			uni.setLocale("zh-Hans");
 		}
-	}
+		console.log(code);
+	},
+	onShow: function () {
+		console.log("App Show");
+	},
+	onHide: function () {
+		console.log("App Hide");
+	},
+};
 </script>
 
 <style lang="less">
 /*每个页面公共css */
 input {
-  margin: 0;
-  bottom: 0;
-  outline: none;
-  border: none;
+	margin: 0;
+	bottom: 0;
+	outline: none;
+	border: none;
 }
 
-.profi-page-container {
-  padding-left: 15rpx;
-  padding-right: 15rpx;
-  box-sizing: border-box;
-  color: #333;
-  font-size: 30rpx;
+.profix-page-container {
+	color: #333;
+	font-size: 30rpx;
+	width: 100vw;
+	height: 100vh;
 
-  .pic {
-    font-size: 0;
+	.page-scroll {
+		padding-left: 15rpx;
+		padding-right: 15rpx;
+		height: 100%;
+	}
 
-    .img {
-      width: 100%;
-    }
-  }
+	.pic {
+		font-size: 0;
 
-  .button {
-    border-radius: 10rpx;
-    border: none;
-    padding: 0;
-    background-color: transparent;
-    line-height: normal;
+		.img {
+			width: 100%;
+		}
+	}
 
-    &::after {
-      content: none;
-    }
-  }
+	.button {
+		border-radius: 10rpx;
+		border: none;
+		padding: 0;
+		background-color: transparent;
+		line-height: normal;
+
+		&::after {
+			content: none;
+		}
+	}
 }
 </style>
