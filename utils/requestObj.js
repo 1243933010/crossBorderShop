e748f16057b1dcf51020b284c5c0ec77fa90573b
yglbhdy <1181330602@qq.com>
2023-11-17 11:17:09 +0800

    充值接口

--- utils/requestObj.js
+++ utils/requestObj.js
@@ -1,13 +1,13 @@
 let requestObj = {
-	region:{
+	region: {
 		name: "注册",
-		url:'/api/register',
-		type:'POST'
+		url: '/api/register',
+		type: 'POST'
 	},
-	login:{
+	login: {
 		name: "登录",
-		url:'/api/login',
-		type:'POST'
+		url: '/api/login',
+		type: 'POST'
 	},
 	vipList: {
 		name: "VIP列表",
@@ -19,47 +19,52 @@ let requestObj = {
 		url: "/api/bank_accounts/index",
 		type: "POST"
 	},
-	
-	bankAccounts:{
-		url:'/api/bank_accounts/index',
-		type:'POST'
+	getRechargeQrCode: {
+		name: "充值获取收款信息",
+		url: "/api/recharge/get_recharge_qr_code",
+		type: "POST"
 	},
-	adverts:{
-		url:'/api/adverts',
-		type:'POST'
+
+	bankAccounts: {
+		url: '/api/bank_accounts/index',
+		type: 'POST'
 	},
-	notices:{
-		url:'/api/notices',
-		type:'POST'
+	adverts: {
+		url: '/api/adverts',
+		type: 'POST'
 	},
-	invitation:{
-		url:'/api/user/invitation',
-		type:'POST'
+	notices: {
+		url: '/api/notices',
+		type: 'POST'
 	},
-	myTeam:{
-		url:'/api/user/my_team',
-		type:'POST'
+	invitation: {
+		url: '/api/user/invitation',
+		type: 'POST'
 	},
-	getUserInfo:{
-		url:'/api/user_info',
-		type:'POST'
+	myTeam: {
+		url: '/api/user/my_team',
+		type: 'POST'
 	},
-	
-	
-	
-	getMailOrderList:{
-		url:'/yoursender/api/mailOrder/getMailOrderList',
-		type:'GET'
-		
+	getUserInfo: {
+		url: '/api/user_info',
+		type: 'POST'
 	},
-	addLogisticOrder:{
-		url:'/yoursender/api/mailOrder/addLogisticOrder',
-		type:'POST'
+
+
+
+	getMailOrderList: {
+		url: '/yoursender/api/mailOrder/getMailOrderList',
+		type: 'GET'
+
 	},
-	wechatLogin:{
-		url:'/yoursender/api/wechat/login',
-		type:'GET',
-		otherGet:true
+	addLogisticOrder: {
+		url: '/yoursender/api/mailOrder/addLogisticOrder',
+		type: 'POST'
+	},
+	wechatLogin: {
+		url: '/yoursender/api/wechat/login',
+		type: 'GET',
+		otherGet: true
 	},
 }
 export default requestObj;
